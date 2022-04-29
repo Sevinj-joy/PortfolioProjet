42d0fdac75e425799245b73b6ade7ff7e0a7b3fd
Sevinj-Joy <seva.rasul15@gmail.com>
2022-04-29 12:21:58 +0400

    JS codes running

--- js/app.js
+++ js/app.js
@@ -6,19 +6,5 @@ window.setInterval(function(){ // 4
     if(currentWord > words.length) currentWord = 0; // 6
     element.textContent = words[currentWord]; // 7
  }, 2000); // -- 4 
-// setTimeout(function(){element.innerHTML+="Sevinc"},4000);
 
-// window.addEventListener("scroll",function(){
-//     let navbar =document.getElementById("navbar-main-div")
-//     navbar.classList.toggle("sticky", window.scrollY > 0);
-// })
-// window.onscroll=function(){myFunction()};
-// let navbar =document.getElementsByClassName("navbar-main-div");
-// let sticky =navbar.offsetTop;
-// function myFunction(){
-//     if(window.pageYOffset >= sticky){
-//         navbar.classList.add("sticky");
-//     }else{
-//         navbar.classList.remove("sticky");
-//     }
-// }
+//  SCroll up and down
