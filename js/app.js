d2b9989770b271a2bb7e51f7c26d8ac11c4f8c79
Sevinj-Joy <seva.rasul15@gmail.com>
2022-06-07 17:22:33 +0400

    Button added

--- js/app.js
+++ js/app.js
@@ -8,3 +8,21 @@ window.setInterval(function(){ // 4
  }, 2000); // -- 4 
 
 //  SCroll up and down
+// Register
+var animateButton = function(e) {
+
+    e.preventDefault;
+    //reset animation
+    e.target.classList.remove('animate');
+    
+    e.target.classList.add('animate');
+    setTimeout(function(){
+      e.target.classList.remove('animate');
+    },700);
+  };
+  
+  var bubblyButtons = document.getElementsByClassName("bubbly-button");
+  
+  for (var i = 0; i < bubblyButtons.length; i++) {
+    bubblyButtons[i].addEventListener('click', animateButton, false);
+  }
