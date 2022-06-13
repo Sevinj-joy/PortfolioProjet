1aca72562c0dfac03ba08b04b3799a0c17d13f61
Sevinj-Joy <seva.rasul15@gmail.com>
2022-06-13 17:35:31 +0400

    Signup html working

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,16 @@
 from xml.etree.ElementInclude import include
-
 from django.http import HttpResponse
+from django import render
+from . import views
 
 def home (request):
     return HttpResponse("Hello Sevinj")
+
+def signup(request):
+    return render(request, 'authentication/signup.html')
+
+def signin(request):
+    return render(request, 'authentication/signin.html')
+
+def signout(request):
+    return render(request, 'authentication/signout.html')
--- django/mysite/urls.py
+++ django/mysite/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from xml.etree.ElementInclude import include
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    # path('', include('authentication.urls')),
-      path('',views.home, name='home')
+    path('admin/', admin.site.urls),
+    path('', include('authentication.urls')),
+      
 ]
