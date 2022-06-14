24c948965af0d9c05dedfc860996dd5d4f2190a5
Sevinj-Joy <seva.rasul15@gmail.com>
2022-06-14 16:24:34 +0400

    Style file working clearly

--- authentication/views.py
+++ authentication/views.py
@@ -4,13 +4,13 @@ from django import render
 from . import views
 
 def home (request):
-    return HttpResponse("Hello Sevinj")
+    return render(request,'index.html')
 
 def signup(request):
     return render(request, 'authentication/signup.html')
 
 def signin(request):
-    return render(request, 'authentication/signin.html')
+    return render(request, 'authentication/login.html')
 
 def signout(request):
     return render(request, 'authentication/signout.html')
